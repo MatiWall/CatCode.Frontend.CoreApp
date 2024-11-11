30d10f5daac2fa1a53b9799d962e7938f7e4498f
Matias Wallenius <MatiWall@live.dk>
2024-11-11 19:19:06 +0100

    initial version of tree resolver, not testet

--- src/app/tree/resolve.ts
+++ src/app/tree/resolve.ts
@@ -19,16 +19,44 @@ function buildFlatMap(extensions: Extension[]): {[parentId: string]: string[]} {
     return idMap;
 }
 
-function buildTree(flatMap: {[parentId: string]: string[]}){
+function findTreeRoot(flatMap: {[parentId: string]: string[]}){
+    /** Find the upper most items */
+    const keys = Object.keys(flatMap);
+    const values = Object.values(flatMap); 
+
+    const children = new Set()
+
+    values.forEach(value => {
+        value.forEach(v => children.add(v))
+    });
     
+
+    return keys.filter(key => !children.has(key))
+}
+
+function buildTree(flatMap: {[parentId: string]: string[]}){
+
+    const roots = findTreeRoot(flatMap);
+
+    function buildNode(parentId: string):{parentId: string, children: object} {
+        
+        const children = flatMap[parentId] || [];
+
+        return {parentId: parentId, children: children.map(key => buildNode(key))}
+
+    }
+
+    return roots.map(key => buildNode(key))
+
 }
 
 function resolveTree(extensions: Extension[]){
 
     const flatMap = buildFlatMap(extensions);
 
+    const tree = buildTree(flatMap);
 
-
+    return tree
 }
 
 
